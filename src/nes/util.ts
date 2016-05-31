4e10d6e71f11ceb7d692eb32d7330ee1ddcf4844
tyfkda <tyfkda@gmail.com>
2016-05-31 21:40:01 +0900

    Use default parameter for hex

--- src/nes/util.ts
+++ src/nes/util.ts
@@ -24,6 +24,7 @@ if (!Uint8Array.prototype.slice) {
 
 export class Util {
   public static hex(x, order) {
+    order = order || 2
     const s = new Array(order)
     for (let i = 0; i < order; ++i) {
       s[order - i - 1] = kHexTbl[x & 0x0f]
