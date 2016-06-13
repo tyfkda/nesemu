02c816483b60758f6b6aa68c9340b730acacc348
tyfkda <tyfkda@gmail.com>
2016-06-14 07:17:39 +0900

    Implement mapper 5d

--- src/nes/mapper/mapper02.ts
+++ src/nes/mapper/mapper02.ts
@@ -1,17 +1,30 @@
+// UxROM
+
 import {Cpu6502} from '../cpu.ts'
 import {Ppu} from '../ppu.ts'
 
-export function mapper02(romData: Uint8Array, cpu: Cpu6502, _ppu: Ppu) {
-  const BANK_SIZE = 16 * 1024
-  const size = romData.length
-  const count = size / BANK_SIZE
-  let prgBank = 0
-  cpu.setReadMemory(0x8000, 0xbfff, (adr) => romData[(adr & (BANK_SIZE - 1)) + prgBank])
-  cpu.setReadMemory(0xc000, 0xffff,
-                    (adr) => romData[(adr & (BANK_SIZE - 1)) + size - BANK_SIZE])
+export function mapper02Creator(prgBankShift) {
+  return function (romData: Uint8Array, cpu: Cpu6502, _ppu: Ppu) {
+    const BANK_BIT = 14
+    const BANK_SIZE = 1 << BANK_BIT
+    const size = romData.length
+    const count = size / BANK_SIZE
+    const kLastBank = size - BANK_SIZE
+    let prgBank = 0
+    cpu.setReadMemory(0x8000, 0xbfff, (adr) => romData[(adr & (BANK_SIZE - 1)) + prgBank])
+    cpu.setReadMemory(0xc000, 0xffff,
+                      (adr) => romData[(adr & (BANK_SIZE - 1)) + kLastBank])
 
-  // PRG ROM bank
-  cpu.setWriteMemory(0x8000, 0xffff, (_adr, value) => {
-    prgBank = (value & (count - 1)) * BANK_SIZE
-  })
+    // PRG ROM bank
+    cpu.setWriteMemory(0x8000, 0xffff, (_adr, value) => {
+      prgBank = ((value >> prgBankShift) & (count - 1)) << BANK_BIT
+    })
+  }
 }
+
+export const mapper02 = mapper02Creator(0)
+
+// INES Mapper 093: Sunsoft-2 IC
+// http://wiki.nesdev.com/w/index.php/INES_Mapper_093
+// This mapper is deprecated for new development. Homebrew projects other than mapper tests should use UxROM (iNES Mapper 002) instead.
+export const mapper5d = mapper02Creator(4)
--- src/nes/mapper/mapper_table.ts
+++ src/nes/mapper/mapper_table.ts
@@ -1,6 +1,6 @@
 import {mapper00} from './mapper00.ts'
 import {mapper01} from './mapper01.ts'
-import {mapper02} from './mapper02.ts'
+import {mapper02, mapper5d} from './mapper02.ts'
 import {mapper03} from './mapper03.ts'
 import {mapper04} from './mapper04.ts'
 
@@ -10,4 +10,6 @@ export const kMapperTable: {[key: number]: Function} = {
   0x02: mapper02,
   0x03: mapper03,
   0x04: mapper04,
+
+  0x5d: mapper5d,  // INES Mapper 093: Sunsoft-2 IC
 }
