b7ae4114085ae66345fd43a2aafe24add0175924
tyfkda <tyfkda@gmail.com>
2016-06-14 20:36:09 +0900

    Reduce memory mapping for mapper 3

--- src/nes/mapper/mapper003.ts
+++ src/nes/mapper/mapper003.ts
@@ -3,8 +3,7 @@ import {Ppu} from '../ppu.ts'
 
 export function mapper003(romData: Uint8Array, cpu: Cpu6502, ppu: Ppu) {
   // ROM
-  cpu.setReadMemory(0x8000, 0xbfff, (adr) => romData[adr & (romData.length - 1)])
-  cpu.setReadMemory(0xc000, 0xffff, (adr) => romData[adr & (romData.length - 1)])
+  cpu.setReadMemory(0x8000, 0xffff, (adr) => romData[adr & (romData.length - 1)])
 
   // Chr ROM bank
   cpu.setWriteMemory(0x8000, 0xffff, (adr, value) => {
