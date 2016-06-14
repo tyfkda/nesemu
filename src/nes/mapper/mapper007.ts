c00376c235e40f7e1c91e408bb792e369f9edf9a
tyfkda <tyfkda@gmail.com>
2016-06-14 19:41:33 +0900

    Implement mapper 7

---
+++ src/nes/mapper/mapper007.ts
@@ -0,0 +1,22 @@
+// AxROM
+
+import {Cpu6502} from '../cpu.ts'
+import {Ppu} from '../ppu.ts'
+
+export function mapper007(romData: Uint8Array, cpu: Cpu6502, _ppu: Ppu) {
+  // 32KB switchable PRG ROM bank
+  const BANK_BIT = 15
+  const BANK_SIZE = 1 << BANK_BIT
+  const size = romData.length
+  const count = size / BANK_SIZE
+  let prgBank = 0
+  cpu.setReadMemory(0x8000, 0xffff, (adr) => romData[(adr & (BANK_SIZE - 1)) + prgBank])
+
+  // PRG ROM bank
+  cpu.setWriteMemory(0x8000, 0xffff, (_adr, value) => {
+    prgBank = (value & (count - 1)) << BANK_BIT
+
+    const namePage = (value >> 4) & 1
+    // TODO: Use namePage
+  })
+}
--- src/nes/mapper/mapper_table.ts
+++ src/nes/mapper/mapper_table.ts
@@ -3,6 +3,7 @@ import {mapper001} from './mapper001.ts'
 import {mapper002, mapper093} from './mapper002.ts'
 import {mapper003} from './mapper003.ts'
 import {mapper004} from './mapper004.ts'
+import {mapper007} from './mapper007.ts'
 import {mapper073} from './mapper073.ts'
 
 export const kMapperTable: {[key: number]: Function} = {
@@ -11,6 +12,8 @@ export const kMapperTable: {[key: number]: Function} = {
   2: mapper002,
   3: mapper003,
   4: mapper004,
+
+  7: mapper007,
 
   73: mapper073,  // INES Mapper 073: Konami VRC3
   93: mapper093,  // INES Mapper 093: Sunsoft-2 IC
