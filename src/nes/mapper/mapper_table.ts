633b82741092a435ebe2ccfb7ba8d24ef5e3438f
tyfkda <tyfkda@gmail.com>
2016-06-24 08:50:21 +0900

    Add mapper 10 (not perfect)

--- src/nes/mapper/mapper_table.ts
+++ src/nes/mapper/mapper_table.ts
@@ -4,6 +4,7 @@ import {mapper002, mapper093} from './mapper002.ts'
 import {mapper003, mapper185} from './mapper003.ts'
 import {mapper004} from './mapper004.ts'
 import {mapper007} from './mapper007.ts'
+import {mapper010} from './mapper010.ts'
 import {mapper023, mapper025} from './mapper023.ts'
 import {mapper024} from './mapper024.ts'
 import {mapper073} from './mapper073.ts'
@@ -15,12 +16,11 @@ export const kMapperTable: {[key: number]: Function} = {
   2: mapper002,
   3: mapper003,
   4: mapper004,
-
   7: mapper007,
+  10: mapper010,
   23: mapper023,
   24: mapper024,
   25: mapper025,
-
   73: mapper073,  // INES Mapper 073: Konami VRC3
   88: mapper004,
   93: mapper093,  // INES Mapper 093: Sunsoft-2 IC
