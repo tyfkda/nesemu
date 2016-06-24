28cd6da1c4a46d396ff3aa35afda63889135132c
tyfkda <tyfkda@gmail.com>
2016-06-24 09:16:00 +0900

    Add mapper 75

---
+++ src/nes/mapper/mapper075.ts
@@ -0,0 +1,60 @@
+// VRC1
+
+import {Cpu} from '../cpu.ts'
+import {Ppu, MirrorMode} from '../ppu.ts'
+import {Util} from '../util.ts'
+
+export function mapper075(romData: Uint8Array, cpu: Cpu, ppu: Ppu) {
+  const BANK_BIT = 13
+  const BANK_SIZE = 1 << BANK_BIT
+  const size = romData.length
+  const count = size / BANK_SIZE
+  const kLastBank = size - BANK_SIZE
+  const prgBank = [kLastBank, kLastBank, kLastBank, kLastBank]
+
+  const chrBank = [0, 0]
+  const setChrBank = (bank, value) => {
+    chrBank[bank] = value
+    const b = bank << 2
+    const ofs = value << 2
+    for (let i = 0; i < 4; ++i)
+      ppu.setChrBankOffset(b + i, ofs + i)
+  }
+
+  cpu.setReadMemory(0x8000, 0xffff,
+                    (adr) => {
+                      const bank = (adr >> 13) & 3
+                      return romData[(adr & (BANK_SIZE - 1)) + prgBank[bank]]
+                    })
+
+  // PRG ROM bank
+  cpu.setWriteMemory(0x8000, 0x9fff, (adr, value) => {
+    if (adr < 0x9000)
+      prgBank[0] = (value & (count - 1)) << BANK_BIT
+    else {
+      ppu.setMirrorMode((value & 1) === 0 ? MirrorMode.VERT : MirrorMode.HORZ)
+      setChrBank(0, (chrBank[0] & 0x0f) | ((value & 2) << 3))
+      setChrBank(1, (chrBank[1] & 0x0f) | ((value & 4) << 2))
+    }
+  })
+  cpu.setWriteMemory(0xa000, 0xbfff, (adr, value) => {
+    if (adr < 0xb000)
+      prgBank[1] = (value & (count - 1)) << BANK_BIT
+  })
+  cpu.setWriteMemory(0xc000, 0xdfff, (adr, value) => {
+    if (adr < 0xd000)
+      prgBank[2] = (value & (count - 1)) << BANK_BIT
+  })
+
+  // CHR ROM bank
+  cpu.setWriteMemory(0xe000, 0xffff, (adr, value) => {
+    const bank = (adr >> 12) & 1
+    setChrBank(bank, (chrBank[bank] & 0x10) | (value & 0x0f))
+  })
+
+  // PRG RAM
+  const ram = new Uint8Array(0x2000)
+  ram.fill(0xbf)
+  cpu.setReadMemory(0x6000, 0x7fff, (adr) => ram[adr & 0x1fff])
+  cpu.setWriteMemory(0x6000, 0x7fff, (adr, value) => { ram[adr & 0x1fff] = value })
+}
--- src/nes/mapper/mapper_table.ts
+++ src/nes/mapper/mapper_table.ts
@@ -8,6 +8,7 @@ import {mapper010} from './mapper010.ts'
 import {mapper023, mapper025} from './mapper023.ts'
 import {mapper024} from './mapper024.ts'
 import {mapper073} from './mapper073.ts'
+import {mapper075} from './mapper075.ts'
 import {mapper184} from './mapper184.ts'
 
 export const kMapperTable: {[key: number]: Function} = {
@@ -22,6 +23,7 @@ export const kMapperTable: {[key: number]: Function} = {
   24: mapper024,
   25: mapper025,
   73: mapper073,  // INES Mapper 073: Konami VRC3
+  75: mapper075,
   88: mapper004,
   93: mapper093,  // INES Mapper 093: Sunsoft-2 IC
   95: mapper004,
