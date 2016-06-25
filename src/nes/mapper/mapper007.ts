1a88791ff5f14243f2ca35bcd5d56334d96de978
tyfkda <tyfkda@gmail.com>
2016-06-25 23:00:56 +0900

    Set mirror mode for mapper 7

--- src/nes/mapper/mapper007.ts
+++ src/nes/mapper/mapper007.ts
@@ -1,9 +1,11 @@
 // AxROM
 
 import {Cpu} from '../cpu.ts'
-import {Ppu} from '../ppu.ts'
+import {Ppu, MirrorMode} from '../ppu.ts'
 
-export function mapper007(romData: Uint8Array, cpu: Cpu, _ppu: Ppu) {
+const kMirrorTable = [MirrorMode.SINGLE0, MirrorMode.SINGLE1]
+
+export function mapper007(romData: Uint8Array, cpu: Cpu, ppu: Ppu) {
   // 32KB switchable PRG ROM bank
   const BANK_BIT = 15
   const BANK_SIZE = 1 << BANK_BIT
@@ -16,7 +18,7 @@ export function mapper007(romData: Uint8Array, cpu: Cpu, _ppu: Ppu) {
   cpu.setWriteMemory(0x8000, 0xffff, (_adr, value) => {
     prgBank = (value & (count - 1)) << BANK_BIT
 
-    // const namePage = (value >> 4) & 1
-    // TODO: Use namePage
+    const namePage = (value >> 4) & 1
+    ppu.setMirrorMode(kMirrorTable[namePage])
   })
 }
