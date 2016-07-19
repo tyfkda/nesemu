afd2c1e286c67241cd8f5458a3eaed0b1066d20a
tyfkda <tyfkda@gmail.com>
2016-07-19 14:21:32 +0900

    Fix calling TSLint
    [gulp-tslint/CHANGELOG.md at master · panuhorsmalahti/gulp-tslint](https://github.com/panuhorsmalahti/gulp-tslint/blob/master/CHANGELOG.md)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -56,8 +56,9 @@ function lint(glob) {
   return gulp.src(glob)
     .pipe(tslint({
       configuration: 'tslint.json',
+      formatter: 'prose',
     }))
-    .pipe(tslint.report('prose', {
+    .pipe(tslint.report({
       emitError: false,
     }))
 }
