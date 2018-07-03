27797a1ecf596521c291b173f8956f5f0b006db7
tyfkda <tyfkda@gmail.com>
2018-07-03 16:06:01 +0900

    Add unittest for Cpu

---
+++ test/nes/cpu.spec.ts
@@ -0,0 +1,31 @@
+import {Cpu} from '../../src/nes/cpu'
+import {Bus} from '../../src/nes/bus'
+import {Address, Byte} from '../../src/nes/types'
+
+class MappedBus extends Bus {
+  constructor(m: {[key: number]: Byte}) {
+    super()
+    this.setReadMemory(0x8000, 0xffff, (adr: Address) => m[adr])
+    this.setWriteMemory(0x8000, 0xffff, (adr: Address, value: Byte) => { m[adr] = value })
+  }
+}
+
+//class MockedBus extends Bus {
+//  constructor(reader: (adr: Address) => Byte,
+//              writer: (adr: Address, value: Byte) => void) {
+//    super()
+//    this.setReadMemory(0x8000, 0xffff, (adr: Address) => reader(adr))
+//    this.setWriteMemory(0x8000, 0xffff, (adr: Address, value: Byte) => { writer(adr, value) })
+//  }
+//}
+
+describe('cpu', () => {
+  it('resets PC from VECTOR', () => {
+    const cpu = new Cpu(new MappedBus({
+      0xfffc: 0xcd,
+      0xfffd: 0xab,
+    }))
+    cpu.reset()
+    expect(cpu.pc).toBe(0xabcd)
+  })
+})
