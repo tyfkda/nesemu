65992fb5d50e788a44c1d9daf8577def4f11b15d
tyfkda <tyfkda@gmail.com>
2018-08-29 20:36:11 +0900

    Add a tool to dump ROM's mapper no

---
+++ tools/getmapperno.ts
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const path = require('path')
+const {promisify} = require('util')
+const jszip = require('jszip')
+
+function getMapperNo(romData: Buffer): number {
+  const NES = 'NES'
+  if (romData[0] != NES.charCodeAt(0) || romData[1] != NES.charCodeAt(1) ||
+      romData[2] != NES.charCodeAt(2) || romData[3] != 0x1a) {
+    console.error('Invalid format')
+    process.exit(1)
+  }
+  return ((romData[6] >> 4) & 0x0f) | (romData[7] & 0xf0)
+}
+
+function dumpMapper(fn: string): void {
+  switch (path.extname(fn).toLowerCase()) {
+  case '.nes':
+    promisify(fs.readFile)(fn)
+      .then(buffer => {
+        console.log(`"${path.basename(fn)}"\tmapper=${getMapperNo(buffer)}`)
+      })
+    break
+  case '.zip':
+    promisify(fs.readFile)(fn)
+      .then(buffer => {
+        const zip = new jszip()
+        return zip.loadAsync(buffer)
+      })
+      .then((loadedZip) => {
+        for (let fileName of Object.keys(loadedZip.files)) {
+          if (path.extname(fileName).toLowerCase() === '.nes') {
+            return loadedZip.files[fileName].async('uint8array')
+          }
+        }
+        return Promise.reject(`${fn}: .nes not included`)
+      })
+      .then(unzipped => {
+        console.log(`"${path.basename(fn)}"\tmapper=${getMapperNo(unzipped)}`)
+      })
+    break
+  default:
+    console.error(`${fn}: Unsupported extname`)
+    process.exit(1)
+    break;
+  }
+}
+
+function main() {
+  const argv = process.argv
+  if (argv.length < 3) {
+    console.error('Please specify .nes or .zip file(s).')
+    process.exit(1)
+  }
+
+  for (let i = 2; i < argv.length; ++i) {
+    dumpMapper(argv[i])
+  }
+}
+
+main()
