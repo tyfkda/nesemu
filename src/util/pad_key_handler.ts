043ed0fa4716e36b12ef393caaa6387a09365f35
tyfkda <tyfkda@gmail.com>
2018-10-01 08:53:47 +0900

    Use computed property keys

--- src/util/pad_key_handler.ts
+++ src/util/pad_key_handler.ts
@@ -1,27 +1,25 @@
 import {PadValue} from '../nes/apu'
 import {KeyCode} from './key_code'
 
-const kKeyTable: {[key: number]: {no: number, bit: number}} = (() => {
-  const t: {[key: number]: {no: number, bit: number}} = {}
-  t[KeyCode.LEFT] = {no: 0, bit: PadValue.L}
-  t[KeyCode.RIGHT] = {no: 0, bit: PadValue.R}
-  t[KeyCode.UP] = {no: 0, bit: PadValue.U}
-  t[KeyCode.DOWN] = {no: 0, bit: PadValue.D}
-  t[KeyCode.Z] = {no: 0, bit: PadValue.B}
-  t[KeyCode.X] = {no: 0, bit: PadValue.A}
-  t[KeyCode.SPACE] = {no: 0, bit: PadValue.SELECT}
-  t[KeyCode.RETURN] = {no: 0, bit: PadValue.START}
+const kKeyTable: {[key: number]: {no: number, bit: number}} = {
+  [KeyCode.LEFT]: {no: 0, bit: PadValue.L},
+  [KeyCode.RIGHT]: {no: 0, bit: PadValue.R},
+  [KeyCode.UP]: {no: 0, bit: PadValue.U},
+  [KeyCode.DOWN]: {no: 0, bit: PadValue.D},
+  [KeyCode.Z]: {no: 0, bit: PadValue.B},
+  [KeyCode.X]: {no: 0, bit: PadValue.A},
+  [KeyCode.SPACE]: {no: 0, bit: PadValue.SELECT},
+  [KeyCode.RETURN]: {no: 0, bit: PadValue.START},
 
-  t[KeyCode.J] = {no: 1, bit: PadValue.L}
-  t[KeyCode.L] = {no: 1, bit: PadValue.R}
-  t[KeyCode.I] = {no: 1, bit: PadValue.U}
-  t[KeyCode.K] = {no: 1, bit: PadValue.D}
-  t[KeyCode.Q] = {no: 1, bit: PadValue.B}
-  t[KeyCode.W] = {no: 1, bit: PadValue.A}
-  t[KeyCode.O] = {no: 1, bit: PadValue.SELECT}
-  t[KeyCode.P] = {no: 1, bit: PadValue.START}
-  return t
-})()
+  [KeyCode.J]: {no: 1, bit: PadValue.L},
+  [KeyCode.L]: {no: 1, bit: PadValue.R},
+  [KeyCode.I]: {no: 1, bit: PadValue.U},
+  [KeyCode.K]: {no: 1, bit: PadValue.D},
+  [KeyCode.Q]: {no: 1, bit: PadValue.B},
+  [KeyCode.W]: {no: 1, bit: PadValue.A},
+  [KeyCode.O]: {no: 1, bit: PadValue.SELECT},
+  [KeyCode.P]: {no: 1, bit: PadValue.START},
+}
 
 export class PadKeyHandler {
   private controller: number[] = [0, 0]
