ba88f025379bc35fe28ede2461a9e25d980192ab
tyfkda <tyfkda@gmail.com>
2019-07-06 09:22:28 +0900

    Separate status register bits
    Avoid bit manipulation for each instruction.

--- src/nes/cpu/cpu.ts
+++ src/nes/cpu/cpu.ts
@@ -18,42 +18,14 @@ const RESERVED_BIT = 5
 const OVERFLOW_BIT = 6
 const NEGATIVE_BIT = 7
 
-const CARRY_FLAG: Byte = 1 << CARRY_BIT
-const ZERO_FLAG: Byte = 1 << ZERO_BIT
-const IRQBLK_FLAG: Byte = 1 << IRQBLK_BIT
-const DECIMAL_FLAG: Byte = 1 << DECIMAL_BIT
 const BREAK_FLAG: Byte = 1 << BREAK_BIT
 const RESERVED_FLAG: Byte = 1 << RESERVED_BIT
-const OVERFLOW_FLAG: Byte = 1 << OVERFLOW_BIT
-const NEGATIVE_FLAG: Byte = 1 << NEGATIVE_BIT
 
 const VEC_NMI: Address = 0xfffa
 const VEC_RESET: Address = 0xfffc
 const VEC_IRQ: Address = 0xfffe
 
 const MAX_STEP_LOG = 200
-
-const _NZ_MASK: Byte = ~(NEGATIVE_FLAG | ZERO_FLAG)
-const _NZC_MASK: Byte = ~(NEGATIVE_FLAG | ZERO_FLAG | CARRY_FLAG)
-
-const kNZTable: Uint8Array = (() => {
-  const table = new Uint8Array(256)
-  for (let i = 0; i < 256; ++i) {
-    let value = 0
-    if (i === 0)
-      value |= ZERO_FLAG
-    if ((i & 0x80) !== 0)
-      value |= NEGATIVE_FLAG
-    table[i] = value
-  }
-  return table
-})()
-
-function setReset(p: Byte, flag: boolean, mask: Byte): number {
-  if (flag)
-    return p | mask
-  return p & ~mask
-}
 
 function inc8(value: Byte): Byte {
   return (value + 1) & 0xff
@@ -81,15 +53,17 @@ export class Cpu {
   private x: Byte  // X register
   private y: Byte  // Y register
   private s: Byte  // Stack pointer
-  private p: Byte  // Status register [NVRBDIZC],
-                   //   N: negative
-                   //   V: overflow
-                   //   R: reserved
-                   //   B: breakmode
-                   //   D: decimal mode
-                   //   I: irq blocked
-                   //   Z: zero
-                   //   C: carry
+
+  // Status register [NVRBDIZC], 0|1 as a boolean
+  private negative = 0
+  private overflow = 0
+  private breakmode = 0
+  private decimal = 0
+  private irqBlocked = 0
+  private reservedFlag = 0
+  private zero = 0
+  private carry = 0
+
   private pc: Address  // Program counter
   private breakPoints: any = {}
   private watchRead: any = {}
@@ -103,14 +77,17 @@ export class Cpu {
   constructor(private bus: Bus) {
     this.$DEBUG = typeof window !== 'undefined' && !!window.$DEBUG  // Accessing global variable!!!
 
-    this.a = this.x = this.y = this.s = this.p = 0
+    this.a = this.x = this.y = this.s = 0
     this.pc = 0
   }
 
   public reset(): void {
-    this.p = IRQBLK_FLAG | BREAK_FLAG | RESERVED_FLAG
     this.s = (this.s - 3) & 0xff
     this.pc = this.read16(VEC_RESET)
+
+    this.negative = this.overflow = this.decimal = this.zero = this.carry = 0
+    this.irqBlocked = this.breakmode = 1
+
     this.stepLogs.length = 0
   }
 
@@ -120,20 +97,13 @@ export class Cpu {
       x: this.x,
       y: this.y,
       s: this.s,
-      p: this.p,
+      p: this.getStatusReg(),
       pc: this.pc,
     }
   }
 
   public save(): object {
-    return {
-      a: this.a,
-      x: this.x,
-      y: this.y,
-      s: this.s,
-      p: this.p,
-      pc: this.pc,
-    }
+    return this.getRegs()
   }
 
   public load(saveData: any): void {
@@ -141,8 +111,9 @@ export class Cpu {
     this.x = saveData.x
     this.y = saveData.y
     this.s = saveData.s
-    this.p = saveData.p
     this.pc = saveData.pc
+
+    this.setStatusReg(saveData.p)
   }
 
   public deleteAllBreakPoints(): void {
@@ -171,9 +142,9 @@ export class Cpu {
       this.addStepLog(`NMI occurred at pc=${Util.hex(this.pc, 4)}`)
     }
     this.push16(this.pc)
-    this.push(this.p & ~BREAK_FLAG)
+    this.push(this.getStatusReg() & ~BREAK_FLAG)
     this.pc = vector
-    this.p |= IRQBLK_FLAG
+    this.irqBlocked = 1
   }
 
   public requestIrq(): void {
@@ -181,7 +152,7 @@ export class Cpu {
   }
 
   public step(): number {
-    if (this.irqDetected && ((this.p & IRQBLK_FLAG) === 0)) {
+    if (this.irqDetected && this.irqBlocked === 0) {
       this.irqDetected = false
       this.handleIrq()
     }
@@ -260,7 +231,7 @@ export class Cpu {
 
     case OpType.ADC:
       {
-        const carry = (this.p & CARRY_FLAG) !== 0 ? 1 : 0
+        const carry = this.carry
         const operand = this.read8(adr)
         const result = this.a + operand + carry
         const overflow = ((this.a ^ result) & (operand ^ result) & 0x80) !== 0
@@ -273,7 +244,7 @@ export class Cpu {
       // The 6502 overflow flag explained mathematically
       // http://www.righto.com/2012/12/the-6502-overflow-flag-explained.html
       {
-        const carry = (this.p & CARRY_FLAG) !== 0 ? 1 : 0
+        const carry = this.carry
         const operand = 255 - this.read8(adr)
         const result = this.a + operand + carry
         const overflow = ((this.a ^ result) & (operand ^ result) & 0x80) !== 0
@@ -340,8 +311,8 @@ export class Cpu {
       {
         const isAcc = inst.addressing === Addressing.ACCUMULATOR
         const value = isAcc ? this.a : this.read8(adr)
-        const oldCarry = (this.p & CARRY_FLAG) !== 0 ? 1 : 0
-        const newCarry = (value & 0x80) !== 0
+        const oldCarry = this.carry
+        const newCarry = value >= 0x80
         const newValue = ((value << 1) | oldCarry) & 0xff
         if (isAcc)
           this.a = newValue
@@ -354,9 +325,9 @@ export class Cpu {
       {
         const isAcc = inst.addressing === Addressing.ACCUMULATOR
         const value = isAcc ? this.a : this.read8(adr)
-        const oldCarry = (this.p & CARRY_FLAG) !== 0 ? 0x80 : 0
+        const oldCarry = this.carry
         const newCarry = (value & 0x01) !== 0
-        const newValue = (value >> 1) | oldCarry
+        const newValue = (value >> 1) | (oldCarry << 7)
         if (isAcc)
           this.a = newValue
         else
@@ -368,7 +339,7 @@ export class Cpu {
       {
         const isAcc = inst.addressing === Addressing.ACCUMULATOR
         const value = isAcc ? this.a : this.read8(adr)
-        const newCarry = (value & 0x80) !== 0
+        const newCarry = value >= 0x80
         const newValue = (value << 1) & 0xff
         if (isAcc)
           this.a = newValue
@@ -394,10 +365,10 @@ export class Cpu {
       {
         const value = this.read8(adr)
         const result = this.a & value
-        this.setZero(result === 0)
+        this.zero = result === 0 ? 1 : 0
 
-        const mask = NEGATIVE_FLAG | OVERFLOW_FLAG
-        this.p = (this.p & ~mask) | (value & mask)
+        this.negative = (value >> NEGATIVE_BIT) & 1
+        this.overflow = (value >> OVERFLOW_BIT) & 1
       }
       break
     case OpType.CMP:
@@ -433,81 +404,81 @@ export class Cpu {
       this.pc = this.pop16() + 1
       break
     case OpType.RTI:
-      this.p = this.pop() | RESERVED_FLAG
+      this.setStatusReg(this.pop() | RESERVED_FLAG)
       this.pc = this.pop16()
       break
 
     case OpType.BCC:
-      cycle += this.branch(adr, (this.p & CARRY_FLAG) === 0)
+      cycle += this.branch(adr, this.carry === 0)
       break
     case OpType.BCS:
-      cycle += this.branch(adr, (this.p & CARRY_FLAG) !== 0)
+      cycle += this.branch(adr, this.carry !== 0)
       break
     case OpType.BPL:
-      cycle += this.branch(adr, (this.p & NEGATIVE_FLAG) === 0)
+      cycle += this.branch(adr, this.negative === 0)
       break
     case OpType.BMI:
-      cycle += this.branch(adr, (this.p & NEGATIVE_FLAG) !== 0)
+      cycle += this.branch(adr, this.negative !== 0)
       break
     case OpType.BNE:
-      cycle += this.branch(adr, (this.p & ZERO_FLAG) === 0)
+      cycle += this.branch(adr, this.zero === 0)
       break
     case OpType.BEQ:
-      cycle += this.branch(adr, (this.p & ZERO_FLAG) !== 0)
+      cycle += this.branch(adr, this.zero !== 0)
       break
     case OpType.BVC:
-      cycle += this.branch(adr, (this.p & OVERFLOW_FLAG) === 0)
+      cycle += this.branch(adr, this.overflow === 0)
       break
     case OpType.BVS:
-      cycle += this.branch(adr, (this.p & OVERFLOW_FLAG) !== 0)
+      cycle += this.branch(adr, this.overflow !== 0)
       break
 
     case OpType.PHA:
       this.push(this.a)
       break
     case OpType.PHP:
-      this.push(this.p | BREAK_FLAG)
+      this.push(this.getStatusReg() | BREAK_FLAG)
       break
     case OpType.PLA:
       this.a = this.pop()
       this.setNZFlag(this.a)
       break
     case OpType.PLP:
-      this.p = this.pop() | RESERVED_FLAG
+      this.setStatusReg(this.pop() | RESERVED_FLAG)
       break
 
     case OpType.CLC:
-      this.p &= ~CARRY_FLAG
+      this.carry = 0
       break
     case OpType.SEC:
-      this.p |= CARRY_FLAG
+      this.carry = 1
       break
 
     case OpType.SEI:
-      this.p |= IRQBLK_FLAG
+      this.irqBlocked = 1
       break
     case OpType.CLI:
-      this.p &= ~IRQBLK_FLAG
+      this.irqBlocked = 0
       break
     case OpType.CLV:
-      this.p &= ~OVERFLOW_FLAG
+      this.overflow = 0
       break
     case OpType.SED:
       // SED: normal to BCD mode
       // not implemented on NES
-      this.p |= DECIMAL_FLAG
+      this.decimal = 1
       break
     case OpType.CLD:
       // CLD: BCD to normal mode
       // not implemented on NES
-      this.p &= ~DECIMAL_FLAG
+      this.decimal = 0
       break
 
     case OpType.BRK:
       this.push16(this.pc + 1)
-      this.push(this.p | BREAK_FLAG)
+      this.push(this.getStatusReg() | BREAK_FLAG)
       this.pc = this.read16(VEC_IRQ)
-      this.p |= IRQBLK_FLAG
+      this.irqBlocked = 1
       break
     }
     // ========================================================
@@ -518,6 +489,28 @@ export class Cpu {
     }
 
     return cycle
+  }
+
+  private getStatusReg(): Byte {
+    return ((this.negative << NEGATIVE_BIT) |
+            (this.overflow << OVERFLOW_BIT) |
+            (this.reservedFlag << RESERVED_BIT) |
+            (this.breakmode << BREAK_BIT) |
+            (this.decimal << DECIMAL_BIT) |
+            (this.irqBlocked << IRQBLK_BIT) |
+            (this.zero << ZERO_BIT) |
+            (this.carry << CARRY_BIT))
+  }
+
+  private setStatusReg(p: Byte): void {
+    this.negative = (p >> NEGATIVE_BIT) & 1
+    this.overflow = (p >> OVERFLOW_BIT) & 1
+    this.reservedFlag = (p >> RESERVED_BIT) & 1
+    this.breakmode = (p >> BREAK_BIT) & 1
+    this.decimal = (p >> DECIMAL_BIT) & 1
+    this.irqBlocked = (p >> IRQBLK_BIT) & 1
+    this.zero = (p >> ZERO_BIT) & 1
+    this.carry = (p >> CARRY_BIT) & 1
   }
 
   private read8(adr: Address): Byte {
@@ -581,20 +574,19 @@ export class Cpu {
 
   // Set N and Z flag for the given value.
   private setNZFlag(nz: Byte): void {
-    this.p = (this.p & _NZ_MASK) | kNZTable[nz]
+    this.zero = nz === 0 ? 1 : 0
+    this.negative = nz >= 0x80 ? 1 : 0
   }
 
   // Set N, Z and C flag for the given value.
   private setNZCFlag(nz: Byte, carry: boolean): void {
-    this.p = (this.p & _NZC_MASK) | kNZTable[nz] | (carry ? CARRY_FLAG : 0)
-  }
-
-  private setZero(value: boolean): void {
-    this.p = setReset(this.p, value, ZERO_FLAG)
+    this.zero = nz === 0 ? 1 : 0
+    this.negative = nz >= 0x80 ? 1 : 0
+    this.carry = carry ? 1 : 0
   }
 
   private setOverFlow(value: boolean): void {
-    this.p = setReset(this.p, value, OVERFLOW_FLAG)
+    this.overflow = value ? 1 : 0
   }
 
   private addStepLog(line: string): void {
@@ -664,9 +656,9 @@ export class Cpu {
       this.addStepLog(`IRQ occurred at pc=${Util.hex(this.pc, 4)}`)
     }
     this.push16(this.pc)
-    this.push(this.p & ~BREAK_FLAG)
+    this.push(this.getStatusReg() & ~BREAK_FLAG)
     this.pc = this.read16(VEC_IRQ)
-    this.p |= IRQBLK_FLAG
+    this.irqBlocked = 1
     return true
   }
 }
