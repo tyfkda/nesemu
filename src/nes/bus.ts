a5355df55c38a41e5a0b9b272b324181d84410ca
tyfkda <tyfkda@gmail.com>
2019-09-28 11:00:31 +0900

    Define reader and writer function types

--- src/nes/bus.ts
+++ src/nes/bus.ts
@@ -3,9 +3,12 @@ import Util from '../util/util'
 
 const BLOCK_SIZE = 0x2000
 
+type Reader = (adr: Address) => Byte
+type Writer = (adr: Address, value: Byte) => void
+
 export class Bus {
-  private readerFuncTable = new Array<(adr: Address) => Byte>(0x10000 / BLOCK_SIZE)
-  private writerFuncTable = new Array<(adr: Address, value: Byte) => void>(0x10000 / BLOCK_SIZE)
+  private readerTable = new Array<Reader>(0x10000 / BLOCK_SIZE)
+  private writerTable = new Array<Writer>(0x10000 / BLOCK_SIZE)
   private readErrorReported = false
   private writeErrorReported = false
 
@@ -14,49 +17,45 @@ export class Bus {
   }
 
   public clearMemoryMap(): void {
-    this.readerFuncTable.fill(_adr => 0xff)
-    this.writerFuncTable.fill((_adr, _value) => {})
-  }
-
-  public setReadMemory(start: Address, end: Address, func: (adr: Address) => Byte): void {
-    const startBlock = (start / BLOCK_SIZE) | 0
-    const endBlock = (end / BLOCK_SIZE) | 0
-    for (let i = startBlock; i <= endBlock; ++i)
-      this.readerFuncTable[i] = func
-  }
-
-  public setWriteMemory(start: Address, end: Address,
-                        func: (adr: Address, value: Byte) => void): void {
-    const startBlock = (start / BLOCK_SIZE) | 0
-    const endBlock = (end / BLOCK_SIZE) | 0
-    for (let i = startBlock; i <= endBlock; ++i)
-      this.writerFuncTable[i] = func
-  }
-
-  public read8(adr: Address): Byte {
-    const block = (adr / BLOCK_SIZE) | 0
-    const reader = this.readerFuncTable[block]
-    if (!reader) {
+    this.readerTable.fill((adr) => {
       if (!this.readErrorReported) {
         console.error(`Illegal read at ${Util.hex(adr, 4)}`)
         this.readErrorReported = true
       }
       return 0xbf  // Returns dummy value (undefined opcode, non plausible value).
-    }
-    return reader(adr)
-  }
-
-  public write8(adr: Address, value: Byte): void {
-    const block = (adr / BLOCK_SIZE) | 0
-    const writer = this.writerFuncTable[block]
-    if (!writer) {
+    })
+    this.writerTable.fill((adr, value) => {
       if (!this.writeErrorReported) {
         const sadr = Util.hex(adr, 4), sv = Util.hex(value, 2)
         console.error(`Illegal write at ${sadr}, ${sv}`)
         this.writeErrorReported = true
       }
-      return
-    }
+    })
+  }
+
+  public setReadMemory(start: Address, end: Address, reader: Reader): void {
+    const startBlock = (start / BLOCK_SIZE) | 0
+    const endBlock = (end / BLOCK_SIZE) | 0
+    for (let i = startBlock; i <= endBlock; ++i)
+      this.readerTable[i] = reader
+  }
+
+  public setWriteMemory(start: Address, end: Address, writer: Writer): void {
+    const startBlock = (start / BLOCK_SIZE) | 0
+    const endBlock = (end / BLOCK_SIZE) | 0
+    for (let i = startBlock; i <= endBlock; ++i)
+      this.writerTable[i] = writer
+  }
+
+  public read8(adr: Address): Byte {
+    const block = (adr / BLOCK_SIZE) | 0
+    const reader = this.readerTable[block]
+    return reader(adr)
+  }
+
+  public write8(adr: Address, value: Byte): void {
+    const block = (adr / BLOCK_SIZE) | 0
+    const writer = this.writerTable[block]
     return writer(adr, value)
   }
 
