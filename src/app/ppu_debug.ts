928e4770997417d82dd14ab55c8d64526e911732
tyfkda <tyfkda@gmail.com>
2020-02-22 09:14:59 +0900

    Pass palette table instead of PPU

--- src/app/ppu_debug.ts
+++ src/app/ppu_debug.ts
@@ -5,18 +5,17 @@ import {kPaletColors} from '../nes/ppu/const'
 
 // No clipping, debug purpose.
 function render8x8Chip(
-  ppu: Ppu, pixels: Uint8ClampedArray, startOffset: number,
-  pattern: Uint16Array, paletHigh: number, clearColor: number, lineWidth: number)
+  pixels: Uint8ClampedArray, startOffset: number, pattern: Uint16Array,
+  paletHigh: number, paletTable: Readonly<Uint8Array>, clearColor: number, lineWidth: number)
 {
   const W = 8
-  const palet = ppu.getPaletTable()
   for (let py = 0; py < W; ++py) {
     const pat = pattern[py]
     for (let px = 0; px < W; ++px) {
       let c = clearColor
       const pal = (pat >> ((W - 1) * 2 - (px << 1))) & 3
       if (pal !== 0) {
-        const col = palet[paletHigh | pal] & 0x3f
+        const col = paletTable[paletHigh | pal] & 0x3f
         c = kPaletColors[col]
       }
 
@@ -34,7 +33,7 @@ export default class PpuDebug {
   {
     const regs = ppu.getRegs()
     const vram = ppu.getVram()
-    const palet = ppu.getPaletTable()
+    const paletTable = ppu.getPaletTable()
     const chrData = ppu.getChrData()
     const hstatusMgr = ppu.getHStatusMgr()
     const mirrorModeBit = hstatusMgr.current.mirrorModeBit
@@ -45,7 +44,7 @@ export default class PpuDebug {
     const W = 8
     const chrStart = getBgPatternTableAddress(regs[PpuReg.CTRL])
 
-    const clearColor = kPaletColors[palet[0] & 0x3f]  // Universal background color
+    const clearColor = kPaletColors[paletTable[0] & 0x3f]  // Universal background color
     const pattern = new Uint16Array(W)
 
     for (let by = 0; by < Const.HEIGHT / W; ++by) {
@@ -63,8 +62,8 @@ export default class PpuDebug {
 
         for (let py = 0; py < W; ++py)
           pattern[py] = getBgPat(chrData, chridx, py, chrBankOffset)
-        render8x8Chip(ppu, pixels, (by * W + startY) * lineWidth + bx * W + startX,
-                      pattern, paletHigh, clearColor, lineWidth)
+        render8x8Chip(pixels, (by * W + startY) * lineWidth + bx * W + startX,
+                      pattern, paletHigh, paletTable, clearColor, lineWidth)
       }
     }
   }
@@ -74,7 +73,7 @@ export default class PpuDebug {
   {
     const W = 8
     const regs = ppu.getRegs()
-    const palet = ppu.getPaletTable()
+    const paletTable = ppu.getPaletTable()
     const chrData = ppu.getChrData()
     const hstatusMgr = ppu.getHStatusMgr()
     const chrBankOffset = hstatusMgr.current.chrBankOffset
@@ -85,15 +84,15 @@ export default class PpuDebug {
     for (let i = 0; i < 2; ++i) {
       const b = i ^ invert
       const paletHigh = ((colorGroups[b] << 2) | (b << 4)) | 0
-      const clearColor = kPaletColors[palet[paletHigh] & 0x3f]
+      const clearColor = kPaletColors[paletTable[paletHigh] & 0x3f]
       const startX = i * (W * 16)
       for (let by = 0; by < 16; ++by) {
         for (let bx = 0; bx < 16; ++bx) {
           const chridx = (bx + by * 16 + i * 256) * 16
           for (let py = 0; py < W; ++py)
             pattern[py] = getBgPat(chrData, chridx, py, chrBankOffset)
-          render8x8Chip(ppu, pixels, (by * W) * lineWidth + bx * W + startX,
-                        pattern, paletHigh, clearColor, lineWidth)
+          render8x8Chip(pixels, (by * W) * lineWidth + bx * W + startX,
+                        pattern, paletHigh, paletTable, clearColor, lineWidth)
         }
       }
     }
