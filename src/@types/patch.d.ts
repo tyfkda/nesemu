e065ab25442c36cc286619e3ae12264b4bf10887
tyfkda <tyfkda@gmail.com>
2020-03-11 08:10:06 +0900

    Specify libs in tsconfig
    Reduct patch declarations.

--- src/@types/patch.d.ts
+++ src/@types/patch.d.ts
@@ -1,27 +1,11 @@
 // Patches for type declarations.
 
 interface Window {
-  File: any
-  FileList: any
-  FileReader: any
-
   mozRequestAnimationFrame: any
   msRequestAnimationFrame: any
 
-  AudioContext: any
   webkitAudioContext: any
-
-  Gamepad: any
 
   app: App
   $DEBUG: boolean
-}
-
-interface HTMLElement {
-  disabled: any
-}
-
-interface CSSStyleDeclaration {
-  imageRendering: string
-  resize: string
 }
