5f7b3869fc20b193f6bf9bd3d4bd6575caf7d138
tyfkda <tyfkda@gmail.com>
2020-05-02 07:40:50 +0900

    Add PPU test

--- test/nes/cpu/cpu.spec.ts
+++ test/nes/cpu/cpu.spec.ts
@@ -1,4 +1,4 @@
-import Cpu from '../../../src/nes/cpu/cpu'
+import Cpu, {IrqType} from '../../../src/nes/cpu/cpu'
 import IBus from '../../../src/nes/cpu/ibus'
 import {Address, Byte} from '../../../src/nes/types'
 
@@ -23,5 +23,35 @@ describe('cpu', () => {
     }))
     cpu.reset()
     expect(cpu.getRegs().pc).toBe(0xabcd)
+  })
+
+  it('NMI', () => {
+    const cpu = new Cpu(new MappedBus({
+      0xfffa: 0x76,
+      0xfffb: 0x98,
+      0xfffc: 0xcd,
+      0xfffd: 0xab,
+    }))
+    cpu.reset()
+    cpu.nmi()
+    expect(cpu.getRegs().pc).toBe(0x9876)
+  })
+
+  it('IRQ', () => {
+    const cpu = new Cpu(new MappedBus({
+      0x8000: 0x58,  // CLI
+      0x8001: 0xea,  // NOP
+      0xc000: 0xea,  // NOP
+
+      0xfffc: 0x00,
+      0xfffd: 0x80,
+      0xfffe: 0x00,
+      0xffff: 0xc0,
+    }))
+    cpu.reset()
+    cpu.step()  // CLI
+    cpu.requestIrq(IrqType.EXTERNAL)
+    cpu.step()
+    expect(cpu.getRegs().pc).toBe(0xc001)
   })
 })
---
+++ test/nes/ppu/ppu.spec.ts
@@ -0,0 +1,39 @@
+import Ppu from '../../../src/nes/ppu/ppu'
+import {PpuReg} from '../../../src/nes/ppu/types'
+
+describe('ppu', () => {
+  it('Vram read is buffered', () => {
+    const ppu = new Ppu()
+
+    ppu.write(PpuReg.CTRL, 0x00)
+    ppu.read(PpuReg.STATUS)  // Reset latch
+    ppu.write(PpuReg.ADDR, 0x20)
+    ppu.write(PpuReg.ADDR, 0x00)
+    ppu.write(PpuReg.DATA, 11)
+    ppu.write(PpuReg.DATA, 22)
+
+    ppu.read(PpuReg.STATUS)  // Reset latch
+    ppu.write(PpuReg.ADDR, 0x20)
+    ppu.write(PpuReg.ADDR, 0x00)
+    ppu.read(PpuReg.DATA)  // Drop first data read
+    expect(ppu.read(PpuReg.DATA)).toBe(11)
+    expect(ppu.read(PpuReg.DATA)).toBe(22)
+  })
+
+  it('Palet read is not buffered', () => {
+    const ppu = new Ppu()
+
+    ppu.write(PpuReg.CTRL, 0x00)
+    ppu.read(PpuReg.STATUS)  // Reset latch
+    ppu.write(PpuReg.ADDR, 0x3f)
+    ppu.write(PpuReg.ADDR, 0x00)
+    ppu.write(PpuReg.DATA, 11)
+    ppu.write(PpuReg.DATA, 22)
+
+    ppu.read(PpuReg.STATUS)  // Reset latch
+    ppu.write(PpuReg.ADDR, 0x3f)
+    ppu.write(PpuReg.ADDR, 0x00)
+    expect(ppu.read(PpuReg.DATA)).toBe(11)
+    expect(ppu.read(PpuReg.DATA)).toBe(22)
+  })
+})
