41e2bcd8727a1a90dd50885a82e9e0f7d9525bcc
tyfkda <tyfkda@gmail.com>
2023-04-20 18:46:45 +0900

    Use single object

--- src/app/global_setting.ts
+++ src/app/global_setting.ts
@@ -2,26 +2,26 @@ import {StorageUtil} from '../util/storage_util'
 
 const KEY_SETTING = 'setting'
 
-export class GlobalSetting {
-  public static pauseOnMenu = false
-  public static muteOnInactive = true
-  public static volume = 0.5
+export const GlobalSetting = {
+  pauseOnMenu: false,
+  muteOnInactive: true,
+  volume: 0.5,
 
-  public static loadFromStorage() {
+  loadFromStorage() {
     const setting = StorageUtil.getObject(KEY_SETTING, null)
     if (setting != null) {
       this.pauseOnMenu = setting.pauseOnMenu
       this.muteOnInactive = setting.muteOnInactive
       this.volume = setting.volume
     }
-  }
+  },
 
-  public static saveToStorage() {
+  saveToStorage() {
     const setting = {
       pauseOnMenu: this.pauseOnMenu,
       muteOnInactive: this.muteOnInactive,
       volume: this.volume,
     }
     StorageUtil.putObject(KEY_SETTING, setting)
-  }
+  },
 }
